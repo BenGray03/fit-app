2c76c70f0157fbdbdddd44faec2cf975b50a1283
BenGray03 <b4n.gray003@gmail.com>
2025-07-25 19:21:17 +0100

    able to get user stats havent made it so it gets passed back however.

--- app/crud.py
+++ app/crud.py
@@ -51,15 +51,30 @@ def create_exercise(db: Session, exercise = schemas.CreateExercise):
     db.refresh(db_input)
     return db_input
 
-def get_bodyweight_history(db: Session, user: schemas.User, skip: int = 0, limit:int = 100):
+def get_bodyweight_history(db: Session, user_id: int, skip: int = 0, limit:int = 100):
     return(
         db.query(models.BodyweightHistory)
-           .filter(models.BodyweightHistory.id == user.id)
+           .filter(models.BodyweightHistory.id == user_id)
            .order_by(models.BodyweightHistory.date.desc())
            .offset(skip)
            .limit(limit)
            .all
            )
+
+def get_latest_bodyweight(db: Session, user_id: int):
+    entry: models.BodyweightHistory = db.query(models.BodyweightHistory)\
+           .filter(models.BodyweightHistory.id == user_id)\
+           .order_by(models.BodyweightHistory.date.desc())\
+           .first
+    return entry.weight if entry else 0
+
+#doesnt get todays nutrition just the latest
+def get_todays_nutrition(db: Session, user_id: int):
+    entry: models.DailyNutrition = db.query(models.DailyNutrition)\
+            .filter(models.DailyNutrition.id == user_id)\
+            .order_by(models.DailyNutrition.date.desc())\
+            .first
+    return entry.protein, entry.calories if entry else 0, 0
 
 def get_bodyweight(db: Session, bodyweight_ID: int):
     return (
--- app/routers/routers.py
+++ app/routers/routers.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from app.database import get_db
 from app.utils import get_current_user
 
-from .. import schemas
+from .. import schemas, crud
 
 router = APIRouter(prefix="")
 
@@ -13,9 +13,12 @@ router = APIRouter(prefix="")
     response_model=schemas.User,
     summary="homepage for every user."
 )
-def home(user: schemas.UserOut = Depends(get_current_user),
+def home(user: schemas.User = Depends(get_current_user),
          db: Session = Depends(get_db)):
-    pass
+    current_bodyweight = crud.get_latest_bodyweight(db, user.id)
+    current_protien, current_calories = crud.get_todays_nutrition(db, user.id)
+
+    
 
 
 
