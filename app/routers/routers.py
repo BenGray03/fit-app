3d008031f18599ffcf7ac7326d47edb2ef71e1b6
BenGray03 <b4n.gray003@gmail.com>
2025-07-25 16:04:04 +0100

    starting to add the home page

--- app/routers/routers.py
+++ app/routers/routers.py
@@ -1,10 +1,21 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app.utils import get_current_user
+
+from .. import schemas
 
 router = APIRouter(prefix="")
 
-
-
-
+@router.get(
+    "/home/",
+    response_model=schemas.User,
+    summary="homepage for every user."
+)
+def home(user: schemas.UserOut = Depends(get_current_user),
+         db: Session = Depends(get_db)):
+    pass
 
 
 
