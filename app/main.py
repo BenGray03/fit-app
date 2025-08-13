9a8a7f8cda4b7ed7d6aabcafcd78360f4c2e6a77
BenGray03 <b4n.gray003@gmail.com>
2025-08-13 14:17:05 +0100

    added the nutrition router

--- app/main.py
+++ app/main.py
@@ -6,12 +6,14 @@ from . import models, schemas, crud
 from .database import engine, get_db
 from .routers.routers import router
 from .routers.authrouter import authRouter
+from .routers.nutrition import nutritionRouter
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login/")
 app.include_router(authRouter)
 app.include_router(router)
+app.include_router(nutritionRouter)
 
 @app.post("/input/", response_model=schemas.UserInputCreate)
 def create_input(user_input: schemas.UserInputCreate, db: Session = Depends(get_db)):
