52ea785bf4f567659bf4f7f237c8e5f4000b66b6
BenGray03 <b4n.gray003@gmail.com>
2025-08-13 12:23:43 +0100

    ok so done a fair amount, made it so that we can now update the daily nutrition, get the data from the specific dates without requiring the user to give out information that could be used maliciously.

---
+++ app/routers/nutrition.py
@@ -0,0 +1,40 @@
+from datetime import date, datetime, timedelta
+from fastapi import APIRouter, Depends, Query
+
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app.utils import get_current_user
+
+from .. import crud, models, schemas, exceptions
+
+nutritionRouter = APIRouter(prefix="/nutrition")
+
+@nutritionRouter.get(
+    "/today/",
+    summary="Get todays goals and goal progress"
+)
+def get_todays_goals(date: date, db: Session = Depends(get_db), current_user:models.User = Depends(get_current_user)):
+    return crud.get_specific_nutrition(db, current_user.id, date)
+
+@nutritionRouter.get(
+    "/history",
+    summary="get previous days goal progress"
+)
+def get_prev_days(start_date: date, days: int = Query(7, ge=1, le=365),db: Session = Depends(get_db), current_user:models.User = Depends(get_current_user)):
+    cuttoff_date = start_date - days
+    return crud.get_range_nutrition(db, current_user.id, start_date, cuttoff_date)
+
+
+@nutritionRouter.patch(
+    "/goals/",
+    response_model=schemas.DailyNutritionOut
+)
+def update_nutrition(payload: schemas.NutritionGoalsPatch, db: Session = Depends(get_db), current_user:models.User = Depends(get_current_user)):
+    if payload.calorie_goal is None and payload.protein_goal is None and payload.calories is None and payload.protein is None:
+        raise exceptions.no_goals_in_payload
+
+    updated_nutrition : models.DailyNutrition = crud.update_nutrition_entry(db=db, user_id=current_user.id, day=payload.date_patch, calories=payload.calories, calorie_goal=payload.calorie_goal, protein=payload.protein, protein_goal=payload.protein_goal)
+    return crud.to_schema(updated_nutrition, schemas.DailyNutritionOut)
+    
+    
+    
